a2182124bfa92b55ea76eb282d4c87adba14c731
byasun <annaclarag6@gmail.com>
2024-11-07 14:53:47 -0300

    Correção URL de conexão

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const usuarioRoutes = require('./routes/usuarioRoutes');
 
 const app = express();
+
+// Permitir CORS para todas as origens
+app.use(cors());
 
 // Conectar ao banco de dados
 connectDB();
