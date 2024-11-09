88944b3ef091cd39142095e962ca4c9eaeb4f53b
byasun <annaclarag6@gmail.com>
2024-11-09 14:20:21 -0300

    teste 4

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ dotenv.config();
 const cors = require('cors');
 const app = require('./app');
 
-const PORT = process.argv.includes("--port=$PORT") ? process.env.PORT : 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
