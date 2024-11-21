d7be354cdbcfd9ad8032e03b5773fc7a142a20c4
Anna Clara Gomes <102702295+byasun@users.noreply.github.com>
2024-11-21 17:50:56 +0000

    teste mongo Atlas

--- config/db.js
+++ config/db.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const conectarDB = async () => {
     try {
+        console.log('Tentando conectar ao MongoDB...');
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
         console.log('Conexão com o MongoDB estabelecida com sucesso!');
     } catch (error) {
-        console.error('Erro ao conectar com o MongoDB', error);
+        console.error('Erro ao conectar com o MongoDB:', error.message);
         process.exit(1);
     }
 };
