cbd746f93473158dc1f4a3853f71495d8ff6e59b
Anna Clara Gomes <102702295+byasun@users.noreply.github.com>
2024-11-26 16:25:50 +0000

    correções de dependencias e cors

--- app.js
+++ app.js
@@ -10,28 +10,34 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+// Middleware para parsing do JSON
 app.use(express.json());
 
 // Conectar ao banco de dados
 conectarDB()
-    .then(() => logger.info('Banco de dados conectado com sucesso.'))
-    .catch((err) => {
-        logger.error('Erro ao conectar ao banco de dados:', { message: err.message });
-        process.exit(1); // Para a aplicação
-    });
+  .then(() => logger.info('Banco de dados conectado com sucesso.'))
+  .catch((err) => {
+    logger.error('Erro ao conectar ao banco de dados:', { message: err.message });
+    process.exit(1); // Para a aplicação
+  });
 
-app.use((req, res, next) => {
-  console.log(`Requisição recebida: ${req.method} ${req.url}`);
-  next(); // Passa para o próximo middleware ou rota
-});
-    
-// Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
+// Configuração de CORS
+const corsOptions = {
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-}));
+};
+app.use(cors(corsOptions));
+
+// Middleware de log de requisições
+app.use((req, res, next) => {
+  console.log(`Requisição recebida: ${req.method} ${req.url}`);
+  if (req.method === 'OPTIONS') {
+    console.log('Requisição OPTIONS recebida.');
+  }
+  next();
+});
 
 // Rota inicial
 app.get('/', (req, res) => {
@@ -41,29 +47,26 @@ app.get('/', (req, res) => {
 
 // Rotas
 app.use('/usuarios', usuarioRoutes);
+console.log('Rotas de usuários carregadas.');
 app.use('/livros', livroRoutes);
 
 // Middleware de tratamento de erros
 app.use(errorHandler);
 
-app.options('*', cors());
-
-app.use((req, res, next) => {
-    logger.info(`Requisição recebida: ${req.method} ${req.url}`);
-    next();
-});
+// Middleware de cookies e sessões
 app.use(cookieParser());
-app.use(session({
-  secret: process.env.JWT_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: process.env.NODE_ENV === 'production', // HTTPS em produção
-    httpOnly: true, // Apenas acessível pelo servidor
-    sameSite: 'None', // Permitir o envio de cookies entre origens diferentes
-  },
-}));
+app.use(
+  session({
+    secret: process.env.JWT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production', // HTTPS em produção
+      httpOnly: true, // Apenas acessível pelo servidor
+      sameSite: 'None', // Permitir o envio de cookies entre origens diferentes
+    },
+  })
+);
 
-
-
+// Exporta o aplicativo
 module.exports = app;
