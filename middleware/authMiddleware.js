408e586699d83ad29aa58f2be59ed231d47b07b5
Anna Clara Gomes <102702295+byasun@users.noreply.github.com>
2024-12-13 15:50:16 +0000

    correção para login

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,11 @@ const jwt = require('jsonwebtoken');
 const logger = require('../utils/logger');
 
 const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization') && req.header('Authorization').split(' ')[1];
+    const authHeader = req.header('Authorization');
+    const token = authHeader && authHeader.split(' ')[1];
+
+    console.log("Header Authorization:", authHeader); // Verifica se o header chega
+    console.log("Token extraído:", token);
 
     if (!token) {
         logger.warn('Token de autenticação não fornecido.');
@@ -12,6 +16,7 @@ const authMiddleware = (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
+
         logger.info(`Usuário autenticado: ${decoded.email}`);
         next();
     } catch (error) {
